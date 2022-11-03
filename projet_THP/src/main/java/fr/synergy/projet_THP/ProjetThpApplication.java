ae0c57d9c0b4e133cd253264468013ce5209b02e
Herve Thomesse <97156081+Treza88@users.noreply.github.com>
2022-11-03 20:02:10 +0100

    debut backend

---
+++ projet_THP/src/main/java/fr/synergy/projet_THP/ProjetThpApplication.java
@@ -0,0 +1,13 @@
+package fr.synergy.projet_THP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetThpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetThpApplication.class, args);
+	}
+
+}
---
+++ projet_THP/src/test/java/fr/synergy/projet_THP/ProjetThpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.synergy.projet_THP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetThpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
