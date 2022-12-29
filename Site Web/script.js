362ee92eec052029eeeb9e7ab88a033fc36616ab
Treza88 <treza88@hotmail.com>
2022-12-29 12:23:31 +0100

    api front

--- Site Web/script.js
+++ Site Web/script.js
@@ -1,17 +1,48 @@
 
-  const ctx = document.getElementById('myChart');
+//function json24(){
+  const container = document.getElementById("container")
+  
+  fetch('http://localhost:8080/api/temps')
+    .then(response => response.json())
+    //talert=commits[1].tempCave);
+    .then(commits => {                                        
+  var tempExt1 =[]
+  var tempInt1 =[]
+  var tempCave1 =[]
+  var dateServer1 =[]
+  for (let r of commits){
+  tempExt1.push(r.tempExt);
+  tempInt1.push(r.tempCave);
+  tempCave1.push(r.dateServer);
+  dateServer1.push(r.tempInt);
+  alert(tempExt1);
+  }
+  //return tempExt1[0];
+  })
+  
+ 
+//}
+//alert(typeof json24())
+alert(tempExt1)
+console.log(json24())
+//var dat=[12, 19, 3, 5, 2, 3]
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var ctx = document.getElementById('myChart');
+  console.log(ctx);
 
   new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Tempetrature des dernière 24 heures',
+        data: tempExt1,
         borderWidth: 1
       }]
     },
     options: {
+      responsive: true,
       scales: {
         y: {
           beginAtZero: true
@@ -19,3 +50,6 @@
       }
     }
   });
+});  
+
+
